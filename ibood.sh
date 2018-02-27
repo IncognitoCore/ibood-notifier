bfaf01b72e465878b4a886f450970b75096c8488
Kevin Broeren <root@kevinbroeren.nl>
2018-02-27 13:40:25 +0100

    Cleaned up title modifications and added support for HTML entities.

--- ibood.sh
+++ ibood.sh
@@ -1,13 +1,13 @@
-title=''
+lastPublishedDeal=''
 suffix="- Internet's Best Online Offer Daily - iBOOD.com"
 while [ true ]
 do
-    var=$(wget -q 'http://www.ibood.com/nl/nl/' -O - | grep -o '<title>.*</title>' | sed 's/\(<title>\|<\/title>\)//g' |sed "s/$suffix$//" | sed -e 's/<[^>]*>//g')
-     if [[ $var != $title ]]
+    currentDeal=$(wget -q 'http://www.ibood.com/nl/nl/' -O - | grep -o '<title>.*</title>' | sed -e 's/<[^>]*>//g' | sed "s@$suffix@@g" | perl -MHTML::Entities -pe 'decode_entities($_);')
+     if [[ $currentDeal != $lastPublishedDeal ]]
      then
-       echo $(date '+%d/%m/%Y %H:%M:%S') "::" $var
-       echo $var | terminal-notifier -appIcon $(pwd)/ibood.png -contentImage $(pwd)/hunt.gif -title 'New iBOOD sale!' -open 'http://www.ibood.com/nl/nl/'
-	     title=$var
+       echo $(date '+%d/%m/%Y %H:%M:%S') "::" $currentDeal
+       echo $currentDeal | terminal-notifier -appIcon $(pwd)/ibood.png -contentImage $(pwd)/hunt.gif -title 'New iBOOD sale!' -open 'http://www.ibood.com/nl/nl/'
+	     lastPublishedDeal=$currentDeal
     fi
     sleep 1
 done
